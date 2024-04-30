718dbdad9b2fcf3db3304df5b97c9ae7ad8053b4
Patricia Medina <patricamedinameneses@gmail.com>
2024-04-30 10:21:16 -0400

    Archivos de inicio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+ Author: Patricia Medina<patriciamedinameneses@gmail.com>
+ Final Test: Bases de javascript
+ Date: 01/05/2024
+ University: USIP
+*/
+
+console.log("Aqui ira todo tu codigo...EXITOS !!!");
+
+const btn = document.getElementById('calcular');
+btn.addEventListener('click', () => {
+  alert('Mensaje de prueba...');
+})
