7a3368e43a3022a99b1a81ea13991987e6759cf6
Patricia Medina <patricamedinameneses@gmail.com>
2024-05-01 18:47:40 -0400

    Mostrando resultado de conversión

--- js/main.js
+++ js/main.js
@@ -5,35 +5,54 @@
  University: USIP
 */
 
-// Usando la API https://www.exchangerate-api.com 
-const URL_CURRENCY = 'https://v6.exchangerate-api.com/v6/e44fd60494acd8e78f880235/latest/BOB'
+// Usando la API https://www.exchangerate-api.com
+const URL_CURRENCY =
+  "https://v6.exchangerate-api.com/v6/e44fd60494acd8e78f880235/latest/BOB";
 
-const SELECT_DE = document.getElementById('de');
-const SELECT_A = document.getElementById('a');
+let url_currency_convert =
+  "https://v6.exchangerate-api.com/v6/e44fd60494acd8e78f880235/pair/";
 
-async function getCurrencies(){
+const SELECT_DE = document.getElementById("de");
+const SELECT_A = document.getElementById("a");
+
+async function getCurrencies() {
   const response = await fetch(URL_CURRENCY);
   const data = await response.json();
   return data;
 }
 
+async function convertCurrency(convertir_de, convertir_a, monto) {
+  url_currency_convert =
+  "https://v6.exchangerate-api.com/v6/e44fd60494acd8e78f880235/pair/";
+  url_currency_convert += convertir_de + "/";
+  url_currency_convert += convertir_a + "/";
+  url_currency_convert += monto;
+  const response = await fetch(url_currency_convert);
+  const data = await response.json();
+  return data.conversion_result;
+}
+
 /* Completando los 2 select con las monedas disponibles al cargar la página*/
-document.addEventListener('DOMContentLoaded', async function(){
+document.addEventListener("DOMContentLoaded", async function () {
   const currencies = await getCurrencies();
-  for (currency in currencies.conversion_rates){
-    SELECT_DE.innerHTML += `<option value=${currency} data-currency=${currency}>${currency}</option>`
+  for (currency in currencies.conversion_rates) {
+    SELECT_DE.innerHTML += `<option value=${currency}>${currency}</option>`;
   }
-  for (currency in currencies.conversion_rates){
-    SELECT_A.innerHTML += `<option value=${currency} data-currency=${currency}>${currency}</option>`
-  } 
-}) 
+  for (currency in currencies.conversion_rates) {
+    SELECT_A.innerHTML += `<option value=${currency}>${currency}</option>`;
+  }
+});
 
-const btn = document.getElementById('calcular');
+const btn = document.getElementById("calcular");
 
-btn.addEventListener('click', () => {
-  const convertir_de = SELECT_DE.getAttribute('data-currency');
-const convertir_a = document.getElementById('a').getAttribute('data-currency');
-console.log(convertir_de)  
-alert(`Mensaje de prueba...${convertir_de}`);
-  
-})
+btn.addEventListener("click", async () => {
+  const convertir_de = document.getElementById("de").value;
+  const convertir_a = document.getElementById("a").value;
+  const monto = document.getElementById("monto").value;
+  const resultado = await convertCurrency(convertir_de, convertir_a, monto);
+  if (resultado) {
+    document.getElementById("respuesta").value = resultado + " " + convertir_a;
+  } else {
+    document.getElementById("respuesta").value = "No logré hacer la conversión";
+  }
+});
